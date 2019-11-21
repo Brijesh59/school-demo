3e35a50a1b6164b75b5def70c576e0c6202622e1
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2019-11-21 09:01:12 +0530

    Create parent.js

---
+++ server/models/parent.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ParentSchema = new Schema({
+    parentId: {
+        type: String,
+        required: true
+    },
+    fatherName: String,
+    motherName: String,
+    fatherOccupation: String,
+    motherOccupation: String,
+    phoneNo: String,
+    nationality: String,
+    presentAddress: String,
+    permanentAddress: String,
+    childs: [ {
+        username: String,
+        password: String,
+        firstName: String
+    }]
+})
+
+ParentSchema.index({ parentId: 1}, { unique: true })
+
+const ParentDetails = mongoose.model('parentdetails', ParentSchema)
+module.exports = ParentDetails
